6d9f448600563cc390c93256ffd9447af1e9b403
Gabriel Alvise <148637079+Alvisera@users.noreply.github.com>
2024-09-04 18:24:33 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Adiciona a classe de animação de entrada
     document.body.classList.add('zooming-in');
 
-    // Remove a classe de animação de entrada após o tempo da animação
     setTimeout(() => {
         document.body.classList.remove('zooming-in');
-    }, 600); // O tempo deve corresponder ao tempo da animação CSS
+    }, 600);
 });
